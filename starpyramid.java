4ff9e49d4326db6edd6ac5d8e8e8ce5fad81074b
Harshal Patel <harshalpateljnv@gmail.com>
2022-10-08 17:26:42 +0530

    Print star pyramid in JAVA

---
+++ starpyramid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class starpyramid {
+
+    // you have to print the following right triangle star pattern.
+    // code by harsh
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+	}
+
+}
